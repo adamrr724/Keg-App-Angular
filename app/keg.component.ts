4e9618228a6ebd255682fde9f037217c21c7085b
Adam Ross Russell and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-23 10:48:01 -0700

    improve layout of keg info

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -6,22 +6,30 @@ import { Keg } from './keg.model';
     inputs: ['keg'],
     outputs: ['onKegPour'],
   template: `
-  <div class="well">
-    <h3>{{ keg.name }}</h3>
-    <h4>ABV: {{ keg.ABV }}%</h4>
-    <ul>
-      <li *ngFor="#type of keg.type">{{ type }}</li>
-    </ul>
-    <h4>Price: \${{ keg.price }}</h4>
-    <h4>Capacity: {{ keg.pints }}/124</h4>
-    <button (click)="kegPour(keg)"
-      name="button"
-      [class.hidden]="keg.pints === 0">
-      Pour Beer
-    </button>
+  <div class="well row">
+    <h3 class="keg__name">{{ keg.name }}</h3>
     <div class="well empty"
-      [class.hidden]="keg.pints !== 0">
-      Out of Beer!
+    [class.hidden]="keg.pints !== 0">
+    Out of Beer!
+    </div>
+    <div class="col-sm-4">
+      <h4>Type:</h4>
+      <ul>
+        <li *ngFor="#type of keg.type">{{ type }}</li>
+      </ul>
+    </div>
+    <div class="col-sm-4">
+      <h4>Info: </h4>
+      <h5>ABV: {{ keg.ABV }}%</h5>
+      <h5>Price: \${{ keg.price }}</h5>
+      <h5>Capacity: {{ keg.pints }}/124</h5>
+    </div>
+    <div class="col-sm-4">
+      <button (click)="kegPour(keg)"
+        name="button"
+        [class.hidden]="keg.pints === 0">
+        Pour Beer
+      </button>
     </div>
   </div>
   `
