1a7123870a498b7381bc89e723530e0d7a532d1d
Adam Ross Russell and Joseph Karasek <adamrr724@yahoo.com>
2016-03-23 08:46:41 -0700

    add model for Keg

---
+++ app/keg.model.ts
@@ -0,0 +1,11 @@
+export class Keg {
+  public pints: number;
+  constructor(
+    public name: string,
+    public type: string[],
+    public ABV: number,
+    public price: number
+  ) {
+    this.pints = 124;
+    }
+}
